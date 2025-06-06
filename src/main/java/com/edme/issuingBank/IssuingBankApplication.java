e33f164cb20867a9f95e3f44252e5a8f21f6671c
Vlad <vladfilimontsev@yandex.ru>
2025-06-06 22:30:55 +0300

    Add Eureka

--- src/main/java/com/edme/issuingBank/IssuingBankApplication.java
+++ src/main/java/com/edme/issuingBank/IssuingBankApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("com.edme.issuingBank.model")
 @EnableJpaRepositories("com.edme.issuingBank.repositories")
 @EnableCaching
+@EnableDiscoveryClient
 public class IssuingBankApplication {
 
 	public static void main(String[] args) {
